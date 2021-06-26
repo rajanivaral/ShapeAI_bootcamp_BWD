f7eecfab3eec5b8e2428846a7b21d4438ccc1a49
rajanivaral <86520920+rajanivaral@users.noreply.github.com>
2021-06-26 17:56:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>needs</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, autem.
+      </p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+}
+
+export default Info;
